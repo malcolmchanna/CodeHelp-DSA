85b00df25bf979a949976e8638a814551d4d44fa
Muzamil Jamil Channa <malcolm.channa@gmail.com>
2022-09-24 03:22:02 +0500

    Add files via upload

---
+++ For&WhileLoop/ForLoopSum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int n ;
+    cout << " enter the value: ";
+     cin >> n;
+    
+    int sum=0;
+      
+     for ( int i = 0; i <= n; i++)
+     {
+        sum += i;
+     cout << " " << sum;
+
+
+     }
+     
+     }
---
+++ For&WhileLoop/whileloop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+int n;
+
+cout << "enter the value";
+cin >> n ;
+
+int i = 1;
+
+while (i<= n)
+{
+   cout << n;
+  i++;
+}
+
+}
